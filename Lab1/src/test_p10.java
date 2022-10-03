8d4957be3bb2ba0862a4eaa8afa897ee950ecac1
xiu1021 <t109360151@ntut.org.tw>
2022-10-03 17:09:44 +0800

    edit

---
+++ Lab1/src/test_p10.java
@@ -0,0 +1,11 @@
+
+public class test_p10 {
+	public class test_10 {
+		
+		public static void main(String[] args) {
+			System.out.println("歡迎使用Java!");
+			System.out.println("開始使用Java吧!");
+		}
+
+	}
+}
---
+++ Lab1/src/test_p19.java
@@ -0,0 +1,11 @@
+
+public class test_p19 {
+	public static void main(String[] args) {
+		System.out.println("十進位的10是"+10);
+		System.out.println("八進位的10是"+010);
+		System.out.println("十六進位的10是"+0x10);
+		System.out.println("十六進位的F是"+0xF);
+
+	}
+
+}
